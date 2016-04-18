5551ecae8ac0eeaa9826edbb6275fc84c821ff2e
bouzuya <m@bouzuya.net>
2016-04-18 20:15:45 +0900

    remove rxjs operators

--- src/run.ts
+++ src/run.ts
@@ -1,13 +1,14 @@
-import { Subject } from 'rxjs';
+import { Subject } from 'rxjs/Subject';
+import { filter } from 'rxjs/operator/filter';
+import { share } from 'rxjs/operator/share';
 import { A } from './a';
 import { Handler } from './handler';
 
 export default function run(app: Handler): void {
   const subject = new Subject<A<any>>();
-  const action$ = subject
-    .asObservable()
-    .filter(action => !!action)
-    .share();
+  const $ = subject.asObservable();
+  const filtered$ = filter.call($, action => !!action);
+  const action$ = share.call(filtered$);
   const re = (action: A<any>) => setTimeout(() => subject.next(action));
   app(action$, { re }).subscribe(re);
 };
